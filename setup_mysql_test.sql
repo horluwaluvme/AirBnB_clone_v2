8609b0b87872792338889d7fd609a77664b6af6d
Opeyemi Alabi <91141140+horluwaluvme@users.noreply.github.com>
2022-10-13 13:33:39 +0100

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,11 @@
--- prepares a MySQL test server for the AirBnb clone
--- creates a database
+-- prepares a MySQL server for the project
+-- GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- create a user and grant privileges
-GRANT ALL ON hbnb_test_db.*
-TO 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
--- grant SELECT privileges
-GRANT SELECT ON performance_schema.*
-TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+USE performance_schema;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+SET FOREIGN_KEY_CHECKS=1;
