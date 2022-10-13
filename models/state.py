7871b3721679555253a87e326e80b9fda8d7d320
Opeyemi Alabi <91141140+horluwaluvme@users.noreply.github.com>
2022-10-13 13:42:03 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from os import getenv
-from sqlalchemy import String, DateTime, Column, ForeignKey
-from sqlalchemy.orm import relationship
-import models
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.city import City
+import models
+from os import getenv
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    __tablename__ = 'states'
+    """ State class """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
+    # else:
+    #     name = ""
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
-    else:
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         @property
         def cities(self):
-            """Getter attribute in case of file storage"""
-            return [city for city in models.storage.all(City).values()
-                    if city.state_id == self.id]
+            """ Getter for cities """
+            state_list = []
+            dic = models.storage.all(City)
+            for city in dic.values():
+                if city.state_id == self.id:
+                    state_list.append(city)
+            return state_list
+
+  
