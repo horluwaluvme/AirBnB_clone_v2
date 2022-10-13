1ab211bff144a8a73d749a568ca72830ce1f0bd1
Opeyemi Alabi <91141140+horluwaluvme@users.noreply.github.com>
2022-10-13 13:40:54 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""This is the city class"""
-from sqlalchemy import String, DateTime, Column, ForeignKey
+# from os import getenv
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+# import models
+# from models.state import State
 
 
 class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
-    """
-    __tablename__ = 'cities'
+    '''
+        Define the class City that inherits from BaseModel.
+    '''
+    __tablename__ = "cities"
+    # if getenv("HBNB_TYPE_STORAGE") == "db":
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', backref='cities',
-                          cascade='all, delete-orphan')
+    places = relationship("Place", backref="cities",
+                              cascade="all, delete")
+    # else:
+    #     state_id = ""
+    #     name = ""
